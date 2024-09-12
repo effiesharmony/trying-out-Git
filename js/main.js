2646758f309fa0ced617710c479018ee5ab709fe
Effie Armony <harmony.effie@gmail.com>
2024-09-12 17:48:21 +0300

    Correct onBallClick()

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict'
 
-function onBallClick(){
+function onBallClick() {
     const elBall = document.querySelector('.ball')
-    elBall.style.backgroundColor = 'blue'
+    var currWidth = elBall.offsetWidth
+    var currHeight = elBall.offsetHeight
+
+    if (currWidth >= 400) {
+        currWidth = 50
+        currHeight = 50
+    }
+    elBall.style.width = `${currWidth + 50}px`
+    elBall.style.height = `${currHeight + 50}px`
+    
+    elBall.innerText = currWidth + 50
 }
